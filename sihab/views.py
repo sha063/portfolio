f7e04adf4630c63e3078e5c02d7e15b2fb9a0e45
sha063 <shihabaaqil22242224@gmail.com>
2023-12-22 18:57:07 +0530

    2

--- sihab/views.py
+++ sihab/views.py
@@ -9,7 +9,17 @@ from django.db import models
 from django.contrib.auth.decorators import login_required,user_passes_test
 
 def sihab(request):
+    class eob():
+        def __init__(self,svg, category, features,price):
+            self.svg = svg
+            self.category = category
+            self.features = features
+            self.price = price
+    e = [ eob('scooter', 'Free', [1, 2, 3, 4],'0.00'),
+        eob('shipped', 'Basic', [1, 2, 3, 4, 5],'9.99'),
+        eob('startup', 'Premium', [1, 2, 3, 4, 5, 6],'99.9')]
     template = loader.get_template('index.html')
     context = {
+        'e':e,
     }
     return HttpResponse(template.render(context, request))
